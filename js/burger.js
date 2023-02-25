6eb5ebeea2f2223ab46c8616eec7b40e1a51a1b7
Vitaly <poroshenkoe@gmail.com>
2023-02-25 23:13:18 +0300

    fix: margins in tel display, fix script, change loading fonts

--- js/burger.js
+++ js/burger.js
@@ -2,9 +2,6 @@ const listBurger = document.querySelector('.nav__list');
 const buttonBurger = document.querySelector('.header__menu');
 const bodyBlock = document.querySelector('body');
 
-console.log(listBurger);
-console.log(buttonBurger);
-
 buttonBurger.addEventListener('click', () => {
   buttonBurger.classList.toggle('active');
   listBurger.classList.toggle('active');
